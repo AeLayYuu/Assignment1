a040601e4442191972b4ad4a560fb9811fbf02c3
Aung Ye Zaw <aungyezaw16316@gmail.com>
2020-08-02 23:36:47 +0630

    update assignment

--- app/src/main/java/com/aungyezaw/myapp/MainActivity.kt
+++ app/src/main/java/com/aungyezaw/myapp/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.lakeexample)
+        setContentView(R.layout.airmash)
     }
 }
